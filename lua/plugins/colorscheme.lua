a370d64f705f69fd7fd1452f73d13c8216239bab
Koji <koji.1127.403@outlook.jp>
2024-02-04 15:38:21 +0900

    modified colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,20 @@
 return {
   { -- colorscheme
-    'folke/tokyonight.nvim',
-    config = function()
-      vim.cmd([[colorscheme tokyonight]])
-    end
+      'folke/tokyonight.nvim',
+  },
+  {
+	'w0ng/vim-hybrid',
+	config = function()
+	  vim.cmd([[ colorscheme hybrid ]])
+	end
+  },
+  {
+      'chriskempson/vim-tomorrow-theme',
+  },
+  {
+      'mhinz/vim-janah',
+  },
+  {
+      'cocopon/iceberg.vim',
   }
 }
